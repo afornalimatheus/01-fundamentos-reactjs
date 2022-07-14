100180d0308415b06b22bf8fabafc2eb49f355fa
Matheus <afornalimatheus@gmail.com>
2022-07-14 08:18:10 -0300

    feat: create avatar component for use on post and comment component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css'
 
@@ -7,7 +8,8 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img className={styles.cover} src="https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/afornalimatheus.png" />
+        {/* <Avatar src="https://github.com/afornalimatheus.png" /> */}
+        <Avatar src="https://github.com/maykbrito.png" />
         <strong>Matheus Afornali</strong>
         <span>Web Developer</span>
       </div>
